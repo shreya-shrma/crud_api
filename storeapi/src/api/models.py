6426ae0a0ccb2b62c04b1931a8a3cff6623b6e31
shreya-shrma <shreya.sharma@paysense.in>
2022-11-22 17:53:41 +0530

    adding src

---
+++ storeapi/src/api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+class Location(models.Model):
+
+    locationName = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return str(self.locationName)
+
+
+class Item(models.Model):
+
+    itemName = models.CharField(max_length=100)
+    dateAdded = models.DateTimeField(auto_now_add=True)
+    itemLocation = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.itemName)
